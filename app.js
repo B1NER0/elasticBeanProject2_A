7d217d5075efad0beb1155fd9c065baa63bc3286
Phillip <degenaarp@gmail.com>
2020-10-13 15:27:51 +0200

    Modified ports

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send("This is the homepage baby");
+    res.send("This is the homepage baby OOOH YEEAH");
 })
 
 app.listen(PORT, () => {
