14b077d51c628c7f6ccfc76cad9efa3249289baa
Phillip <degenaarp@gmail.com>
2020-10-21 15:31:19 +0200

    Readded mongo

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-//const MongoClient = require('mongodb').MongoClient;
-//const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-//const url = 'mongoedb://3.84.45.74'
+const url = 'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';  //'mongodb+srv://Binero:Alfadelta4@cluster0mongodb.nwadf.mongodb.net/sample_airbnb?retryWrites=true&w=majority';//'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';
 
+MongoClient.connect(url, (err, database) => {
+    if (err) return console.log(err);
+    db = database.db("Admin");
+    console.log("SDF");
+});
 
 
 app.get('/', (req, res) => {
-    res.send("This is the homepage baby OOOH YEEAH");
-})
+    res.send("This is the homepage baby OOOH hhYEEAH");
+});
+    
 
 app.listen(PORT, () => {
     console.log("Server is running...");
 
-    
-    
-
-})
+});
 
