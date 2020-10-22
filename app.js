8c2256d6080ec29e2e65919d6ced99ce7421ff34
Phillip <degenaarp@gmail.com>
2020-10-22 09:40:59 +0200

    added res.send line

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ const assert = require('assert');
 
 const url = 'mongodb://thisIsTheUser:1234@3.84.45.74/TestDB?retryWrites=true&w=majority';  //'mongodb+srv://Binero:Alfadelta4@cluster0mongodb.nwadf.mongodb.net/sample_airbnb?retryWrites=true&w=majority';//'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';
 
+falg = false;
+
 MongoClient.connect(url, (err, database) => {
     if (err) return console.log(err);
     db = database.db("TestDB");
     console.log("SDF");    
+
+    flag = true;
 });
 
 
 app.get('/', (req, res) => {
-    res.send("This is the homepage baby OOOH hhYEEAH");
+    res.send("This is the homepage baby OOOH hhYEEAH" + flag);
 });
     
 
