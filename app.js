72555246de05d03d115a26b4adb0c677ebccf4a3
Phillip <degenaarp@gmail.com>
2020-10-22 11:01:46 +0200

    Added home get function

--- app.js
+++ app.js
@@ -4,25 +4,29 @@ const PORT = process.env.PORT || 8080;
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const { json } = require('express');
 
 const url = 'mongodb://thisIsTheUser:1234@3.84.45.74/TestDB?retryWrites=true&w=majority';  //'mongodb+srv://Binero:Alfadelta4@cluster0mongodb.nwadf.mongodb.net/sample_airbnb?retryWrites=true&w=majority';//'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';
 
-falg = false;
-
-MongoClient.connect(url, (err, database) => {
-    if (err) return console.log(err);
-    db = database.db("TestDB");
-    console.log("SDF");    
-
-    flag = true;
-});
 
 
 app.get('/', (req, res) => {
-    res.send("This is the homepage baby OOOH hhYEEAH" + flag);
-});
-    
+    //res.send("This is the homepage baby OOOH hhYEEAH");
 
+    MongoClient.connect(url, (err, database) => {
+        if (err) return console.log(err);
+        var db = database.db("TestDB");
+        db.collection("testData").findOne({
+            _id: "3"
+        },
+        function(err, result){
+            if(err) return console.log(err);
+            res.send(result.color);
+            console.log(result.color); 
+            
+    });
+});
+});
 app.listen(PORT, () => {
     console.log("Server is running...");
 
