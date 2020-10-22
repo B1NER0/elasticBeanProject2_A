26647261dbc910d2246ae3a8cf396e868aba2919
Phillip <degenaarp@gmail.com>
2020-10-22 09:28:51 +0200

    Added new user to mongodb

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 8080;
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';  //'mongodb+srv://Binero:Alfadelta4@cluster0mongodb.nwadf.mongodb.net/sample_airbnb?retryWrites=true&w=majority';//'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';
+const url = 'mongodb://thisIsTheUser:1234@3.84.45.74/TestDB?retryWrites=true&w=majority';  //'mongodb+srv://Binero:Alfadelta4@cluster0mongodb.nwadf.mongodb.net/sample_airbnb?retryWrites=true&w=majority';//'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';
 
 MongoClient.connect(url, (err, database) => {
     if (err) return console.log(err);
-    db = database.db("Admin");
+    db = database.db("TestDB");
     console.log("SDF");    
 });
 
