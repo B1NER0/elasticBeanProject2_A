c947f46bf35587d87f5870fc3dece983203dc604
Phillip <degenaarp@gmail.com>
2020-11-04 14:36:44 +0200

    First commit

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const { json } = require('express');
 
-const url = 'mongodb://username:password@3.84.36.77/thisOne?retryWrites=true&w=majority';  //'mongodb+srv://Binero:Alfadelta4@cluster0mongodb.nwadf.mongodb.net/sample_airbnb?retryWrites=true&w=majority';//'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';
+const url = 'mongodb://username:password@3.84.36.77/thisOne?retryWrites=true&w=majority'; 
 
 
 
-app.get('/test', (req, res) => {
+app.get('/', (req, res) => {
     //res.send("This is the homepage baby OOOH hhYEEAH");
 
     MongoClient.connect(url, (err, database) => {
@@ -22,9 +22,7 @@ app.get('/test', (req, res) => {
         function(err, result){
             if(err) return console.log(err);
             res.send(result.name);
-            console.log(result.name); 
-           console.log("ASDF");
-            
+            console.log(result.name);             
     });
 });
 });
